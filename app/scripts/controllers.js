17e312fb4fedf57a28c238906b3eb0fa220df9ae
nawanshu <nawanshu@gmail.com>
2017-05-17 18:10:07 +0530

    Wrap controller scope variable inside object and few other code cosmetic changes

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -9,48 +9,39 @@ angular.module('weatherApp.controllers', [])
     ['$scope','weatherSvc','ISO3166',
       function($scope,openWeatherMap,ISO3166) {
 
-    $scope.message = '';
-    $scope.hasState = '';
+    $scope.weatherData={
+        message : '',
+        forecast : '',
+        iconBaseUrl : 'http://openweathermap.org/img/w/',
+        location: ''
+    }      
+          
+     var wd = $scope.weatherData; 
 
-    $scope.myObj = {
-    "color" : "black",
-    "background-color" : "coral",
-    "font-size" : "60px",
-    "padding" : "50px"
-  }
-    $scope.defaultBackground = {
+    //We can enhance this application by adding background image using ng-style      
+    /*$scope.defaultBackground = {
         background: 'url(./img/weather.jpg)'
-    }    
+    }  */  
     
-    var wd = $scope.weatherData; 
-    
-    $scope.iconBaseUrl = 'http://openweathermap.org/img/w/';
-
     // On initialization load data for first example entry
-    $scope.forecast = openWeatherMap.queryForecastDaily({
+    wd.forecast = openWeatherMap.queryForecastDaily({
       location: "Pune"
     });
 
     // Get forecast data for location as given in $scope.location
     $scope.getForecastByLocation = function() {
-
-      if ($scope.location == '' || $scope.location == undefined) {
-        $scope.hasState = 'has-warning';
-        $scope.message = 'Please provide a location';
+      if (wd.location == '' || wd.location == undefined) {
+        wd.message = 'Please provide a location';
         return;
       }
-
-      $scope.hasState = 'has-success';
-
-      $scope.forecast = openWeatherMap.queryForecastDaily({
-        location: $scope.location
+      wd.forecast = openWeatherMap.queryForecastDaily({
+        location: wd.location
       });
-     console.log($scope.forecast);
     };
 
     // Get icon image url
     $scope.getIconImageUrl = function(iconName) {
-      return (iconName ? $scope.iconBaseUrl + iconName + '.png' : '');
+      return (iconName ? wd.iconBaseUrl + iconName + '.png' : '');
     };
 
   }])
--- app/scripts/services.js
+++ app/scripts/services.js
@@ -6,7 +6,6 @@ angular.module('weatherApp.services', ['ngResource'])
 
   .factory('weatherSvc', function($resource) {
 
-    // API key is currently unused (work either with or without key)
     var apiKey = '279b4be6d54c8bf6ea9b12275a567156';
     var apiBaseUrl = 'http://api.openweathermap.org/data/2.5/';
 
@@ -26,10 +25,7 @@ angular.module('weatherApp.services', ['ngResource'])
             subPath: 'daily',
             cnt: 7
           },
-          isArray: false,
-          headers: {
-            'x-api-key': apiKey
-          }
+          isArray: false
         }
       }
     )
